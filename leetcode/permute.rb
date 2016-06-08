5e585bbed0e3b0dab90e58cc0e958a6a9edbfbee
angelfan <gavin.geng@commandp.com>
2016-06-08 11:37:08 +0800

    add(leetcode): combination and permutation

--- leetcode/permute.rb
+++ leetcode/permute.rb
@@ -2,5 +2,41 @@ def permute(nums)
   nums.permutation.to_a
 end
 
-nums = [1,2,3]
-p permute(nums)
+# nums = [1,2,3,4]
+# p permute(nums)
+
+
+def permutation(nums, flag = false)
+  dup_nums = nums.dup
+  permutation = [Array.new([dup_nums.shift])]
+  dup_nums.each do |n|
+    permutation.dup.each do |v|
+      (v.size+1).times do |index|
+        permutation << v.dup.insert(index, n)
+      end
+    end
+  end
+  if flag
+    permutation.select!{|arr| arr.size == nums.size}
+  end
+  permutation
+end
+
+p permutation([1])
+p permutation([1,2])
+p permutation([1,2,3], true)
+# [1] = 1
+# [1, 2] = [1,2,] [2,1]
+# [1,2,3] = [1,2,3]
+#
+#
+# [1,2,3,4] 1, [2,3,4]    1234 2134 2314 2341
+#
+# 1
+#
+# 12 21
+#
+# 123 132 231 213 321 312
+#
+# 1234 1243 1324 1342 1432 1423 2134 2143 2314 2341 2413 2431
+# 3124 3142 3241 3214 3412 3421 4123 4132 4213 4231 4312 4321
--- leetcode/test.rb
+++ leetcode/test.rb
@@ -71,4 +71,50 @@ def new_three_columns(arr, columns)
   puts_arr.each { |arr| puts arr.join(' ') }
 end
 
-new_three_columns([1, 2, 3, 4, 5, 6, 7, 8], 3)
+# new_three_columns([1, 2, 3, 4, 5, 6, 7, 8], 3)
+
+
+#
+# [1] => 1
+# [1,2] => 1 2 12
+# [1,2,3] => 1 2 12 13 23 123 3
+#
+#
+# [1,2,3,4] =
+# n=1 = 1
+# n=2 = 1 * 2 + 1
+# n=3 = (1 * 2 + 1) * 2 + 1
+#
+# [1,2, 3]
+# 1 2 3
+# 1 2 13 23
+# 123
+
+def new_combination(arr)
+  combination = []
+  arr.each do |i|
+    combination.dup.each do |v|
+      combination << v.dup.push(i)
+    end
+    combination << Array.new([i])
+  end
+  combination
+end
+
+p new_combination([1,2,3])
+
+
+def combination_with_sum(arr)
+  combination = Hash.new
+  arr.each do |i|
+    combination.dup.each do |k, v|
+      combination[k.dup.push(i)] = v+i
+    end
+    combination[Array.new([i])] = i
+  end
+  combination
+end
+
+p combination_with_sum([1,2,3])
+
+
