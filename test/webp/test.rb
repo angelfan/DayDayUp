222a9133f0c5bd410476a65e6a714d5223e06b29
angelfan <gavin.geng@commandp.com>
2016-11-04 14:26:27 +0800

    test: process image

--- test/webp/test.rb
+++ test/webp/test.rb
@@ -51,3 +51,22 @@ result = first_image.composite(second_image) do |c|
   c.geometry 'Center'
 end
 result.write "output.jpg"
+system `convert -size 300x40 xc:none -background transparent -fill '#ce201a'-draw' rectangle 0,0, 300,20' -shear 32 Packt_logo.gif`
+#
+# width * height
+#
+# “-draw "text 68,147 'PUBLISHING'" ”
+#
+# system "convert -size #{width}x#{height} xc:#{color} #{format}:#{tempfile.path}"
+#
+#     system `convert ad.jpg -fill orange -pointsize 80 -draw "text 20,95 'PACKT'" -fill black -pointsize 27 -draw "text 68,147 'PUBLISHING'" Packt_logo.gif`
+require 'tempfile'
+width = 200
+height = 300
+system "convert -size #{width}x#{height} xc:none -bordercolor '#b0cdfa' -border 2  PNG32:xx.png"
+#
+
+# color = 'blue'
+# label = 'nih1ao'
+#
+# system 'convert -size 20x20 -gravity Center -background none -fill black -strokewidth 2 -stroke red -pointsize 56 caption:@capt.txt out.png'
